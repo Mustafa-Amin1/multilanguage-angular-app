243d65bf52ee54ee3bbe3c493828bc28fcc36ca4
Mustafa-Amin1 <acc.mustafa1@yahoo.com>
2021-07-01 13:56:17 +0200

    update home page

--- src/app/home-page/components/our-banks/our-banks.component.ts
+++ src/app/home-page/components/our-banks/our-banks.component.ts
@@ -13,65 +13,90 @@ export class OurBanksComponent implements OnInit {
   ngOnInit(): void {
     this.bankMarks =[
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/banks/5eb30b197b0bc1588792089.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/banks/5eb32ecaac62d1588801226.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/banks/5eb313e41ebad1588794340.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/banks/5eb3149e192091588794526.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/banks/5eb31448c4a7a1588794440.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/banks/5f0ca542100c31594664258.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb32a2ac42051588800042.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb32b230b41a1588800291.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb32f56994e71588801366.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb32fa725a7f1588801447.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb33a06908e01588804102.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb33f7fad6fe1588805503.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb332e45bd5a1588802276.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb335e7a11851588803047.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb336e794b761588803303.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb340a19bd201588805793.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb341c3aaac51588806083.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb3312a45f361588801834.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb3350be4e5b1588802827.svg',
       },
       {
-        bankMarkImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 30.png',
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb3358c4ba6d1588802956.svg',
       },
+      {
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb3366a41f1e1588803178.svg',
+      },
+      {
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb3380fee4431588803599.svg',
+      },
+      {
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb3412a1cafa1588805930.svg',
+      },
+      {
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb3422e228491588806190.svg',
+      },
+      {
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb33325a83f61588802341.svg',
+      },
+      {
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb33772d3e551588803442.svg',
+      },
+      {
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb3387422f881588803700.svg',
+      },
+      {
+        bankMarkImgSrc: '../../../../assets/images/finance-agancies/5eb34293c65311588806291.svg',
+      },
+
     ]
   }
   // form text box height 100%
--- src/app/home-page/components/our-clients/our-clients.component.ts
+++ src/app/home-page/components/our-clients/our-clients.component.ts
@@ -12,64 +12,94 @@ public agancyImgs =[]
   ngOnInit(): void {
     this.agancyImgs =[
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5c9ceee06184a1553788640.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5ca8e47aee4661554572410.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5cac59dd94bf71554799069.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5cadac1a432af1554885658.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5caf141da42881554977821.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5cb2d8529843f1555224658.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5cb5b1c4e0aab1555411396.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5cb82caa8c0551555573930.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5cbc1b1c8fde31555831580.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5cbc3c3ccd0b51555840060.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5cc959329fb9a1556699442.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5cd87c5bc46c91557691483.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5ce9306fbb95e1558786159.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5cf3d0f2c58bf1559482610.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5d0de79b5d5541561192347.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5d5d0bd1a60991566378961.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5d18bcd7b1fb51561902295.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5d60db52bdff71566628690.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5d67a0c97dc181567072457.svg',
       },
       {
-        agancyImgSrc: '../../../../assets/images/home-page/caroffers-carousel/Rectangle 35.png',
+        agancyImgSrc: '../../../../assets/images/marks/5daf3596afa3b1571763606.svg',
+      },
+      {
+        agancyImgSrc: '../../../../assets/images/marks/5e5f839e875321583317918.svg',
+      },
+      {
+        agancyImgSrc: '../../../../assets/images/marks/5e53cea14ee651582550689.svg',
+      },
+      {
+        agancyImgSrc: '../../../../assets/images/marks/5e54d1712b32c1582616945.svg',
+      },
+      {
+        agancyImgSrc: '../../../../assets/images/marks/5e54d56954ddc1582617961.svg',
+      },
+      {
+        agancyImgSrc: '../../../../assets/images/marks/5e537e28140781582530088.svg',
+      },
+      {
+        agancyImgSrc: '../../../../assets/images/marks/5e538b83b69681582533507.svg',
+      },
+      {
+        agancyImgSrc: '../../../../assets/images/marks/5e5378b233cb91582528690.svg',
+      },
+      {
+        agancyImgSrc: '../../../../assets/images/marks/5e5382d83439e1582531288.svg',
+      },
+      {
+        agancyImgSrc: '../../../../assets/images/marks/5e5390c39c5e01582534851.svg',
+      },
+      {
+        agancyImgSrc: '../../../../assets/images/marks/5e513903cbefd1582381315.svg',
       },
     ]
   }
